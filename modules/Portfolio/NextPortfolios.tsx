d59d7e79c451a3df357779c799ac67dec975b662
tho trang <nguyenanhtuan23122000@gmail.com>
2024-05-28 10:40:01 +0700

    continue

--- modules/Portfolio/NextPortfolios.tsx
+++ modules/Portfolio/NextPortfolios.tsx
@@ -1,6 +1,5 @@
 import BaseLayoutWraper from "@/components/BaseLayoutWraper";
 import BasePagination from "@/components/BasePagination";
-import { useRouter } from "next/router";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "stores/store";
@@ -14,17 +13,19 @@ export default function NextPortfolios() {
     if (size === ESizeScreen.SM || size === ESizeScreen.XS) return 1;
     return 3;
   }, [size]);
-  const router = useRouter();
-  const portfolioId = Number(router.query.id?.[1]);
-  const { portfolios } = useSelector((s: RootState) => s.data_store);
+  const { portfolios, portfolioDetail } = useSelector(
+    (s: RootState) => s.data_store
+  );
+
+  const currentId = portfolioDetail.id;
   const listData = useMemo(() => {
-    if (!portfolioId || !portfolios) return [];
-    let list = portfolios.filter((item) => item.id > portfolioId);
+    if (!currentId || !portfolios) return [];
+    let list = portfolios.filter((item) => item.id > currentId);
     if (list.length < 3) list = [...list, ...portfolios];
     const sets = new Set(list);
     return [...sets].slice(0, 3);
-  }, [portfolioId, portfolios]);
-  
+  }, [currentId, portfolios]);
+
   return (
     <BaseLayoutWraper className="2xl:py-base100 lg:py-base80 py-base60 !px-8">
       <BasePagination
---
+++ pages/portfolio_detail/[id].tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import Layout from "../../layouts";
+import ListCategory from "@/modules/ListCategory";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "stores/store";
+import { useRouter } from "next/router";
+import BannerPortfolioDetail from "@/modules/BannerPortfolioDetail";
+import GridImagePortfolio from "@/modules/Portfolio/GridImagePortfolio";
+import ContactFormGroup from "@/modules/ContactUs/ContactGroup";
+import InforPortfolio from "@/modules/Portfolio/InforPortfolio";
+import NextPortfolios from "@/modules/Portfolio/NextPortfolios";
+
+export default function PortfolioDetail() {
+  const { data_store } = useDispatch<Dispatch>();
+  const { getPortfolio, getPortfolios, getServices } = data_store;
+  const router = useRouter();
+  const portfolioId = router.query.id;
+
+  useEffect(() => {
+    if (portfolioId) getPortfolio(portfolioId);
+    getPortfolios({});
+    getServices({});
+  }, [portfolioId]);
+  return (
+    <Layout>
+      <BannerPortfolioDetail />
+      <InforPortfolio />
+      <GridImagePortfolio />
+      <NextPortfolios />
+      <ContactFormGroup />
+      <ListCategory />
+    </Layout>
+  );
+}
